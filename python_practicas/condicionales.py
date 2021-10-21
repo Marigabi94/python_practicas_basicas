b32927e81ce07b077c3ac2cb91a1d506a2113079
Marigabi94 <marigabi940@gmail.com>
2021-10-21 12:41:37 -0400

    Practica de condicionales

---
+++ python_practicas/condicionales.py
@@ -0,0 +1,13 @@
+"""
+Practica #6 condicionales
+
+"""
+
+num = int(input('Ingrese un numero: \n'))
+
+if num%2 ==0:
+    print('El numero es par')
+elif num>80: 
+    print('El numero es impar mayor que 80')
+else:
+    print('El numero es impar')
