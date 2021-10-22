1ae545ad4626f6a5c8572d668956eb53632d575e
Marigabi94 <marigabi940@gmail.com>
2021-10-22 19:07:45 -0400

    Practica #9 Tuplas

---
+++ python_practicas/tuplas.py
@@ -0,0 +1,16 @@
+"""
+
+Practica #9 Tuplas
+
+"""
+
+tupla= (1,2,3,4,5,6,7,8,9)
+tupla2=(2,3,'cadena',True,10.5)
+tupla3=(2,3,['cadena',True,10.5], tupla)
+
+
+
+
+print(tupla,'\n',tupla2,'\n',tupla3)
+print(tupla2[3])
+print(tupla3[2])
