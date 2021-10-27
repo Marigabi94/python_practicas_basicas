c6435c8545c17b2a485f5552f1300319889969d7
Marigabi94 <marigabi940@gmail.com>
2021-10-27 16:19:07 -0400

    Practica #10 ciclo for

---
+++ python_practicas/for.py
@@ -0,0 +1,16 @@
+"""
+Practica #10 Ciclo For
+
+"""
+
+lista = ['Hola','Palabra',2,'Facebook']
+
+"""
+        for variable in ESTRUCTURA:
+            instrucciones a ejecutar
+
+
+"""
+
+for i in lista:
+    print(i) 
