af65605e2213265e6f0d812129e42ca122714924
Marigabi94 <marigabi940@gmail.com>
2021-12-04 14:42:25 -0400

    practica de funiones en tuplas

---
+++ python_practicas/funciones_tuplas.py
@@ -0,0 +1,20 @@
+"""
+
+Practica de funciones con tuplas.
+
+"""
+
+
+#TODO: Imprime el tercere elemento de la tupla
+tupla = (1,2,3,4,5,6,7,8,9,10)
+print(tupla.index(3))
+
+
+#TODO: Cuenta la cantidad de elementos que sean número 8.
+tupla2 = (1,2,3,4,5,6,7,8,8,8,8,8,8,9,10)
+print(tupla2.count(8))
+
+#TODO: borrar por completo la variable tupla, si se imprime tupla luego de utilizar "del" dara error ya que fue eliminada y no existe.
+del tupla
+
+
